518de02b5c5a66a627406153d712f92e1145459d
anavitoriaq <anavitoriaqueiroz78@gmail.com>
2024-07-23 15:28:05 -0300

    att

--- Unidade_I/alocação_dinamica/lista/q2.c
+++ Unidade_I/alocação_dinamica/lista/q2.c
@@ -4,13 +4,14 @@ questões e o gabarito da prova. Em seguida, serão lidas as respectivas respost
 10 alunos matriculados. Calcule e mostre:
 a) a nota obtida para cada aluno;
 b) a porcentagem de aprovação, sabendo-se que a nota mínima para ser aprovado é 6.*/
+#include<stdio.h>
 
 int main(void){
     int quantidadeQuestoes, alunos = 10, contador;
     float notas[alunos];
     // primeiro dado: quantidade de questões
     printf("informe a quantidade de questoes:");
-    scanf("%d", &quantidadeQuestoes);
+    scanf(" %d", &quantidadeQuestoes);
     // cada questão vale 10/N pontos
     float pontos = 10.0/ quantidadeQuestoes;
     char gabarito [quantidadeQuestoes];
@@ -18,6 +19,6 @@ int main(void){
     //segundo dado: gabarito da prova
     for(contador = 0; contador < quantidadeQuestoes; contador++){
         printf("digite o gabarito da questao %d: ", contador+1);
-        scanf("%c", &gabarito[contador]);
+        scanf(" %c", &gabarito[contador]);
     }
 }
